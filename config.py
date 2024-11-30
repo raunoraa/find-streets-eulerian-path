2e688323556f342d255965e54dfb6e73d9dafa85
Rauno Raa <raunoraa13@gmail.com>
2024-12-01 01:27:38 +0200

    Fixed a small bug.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configuration file for file paths and constants used throughout the application.
 
 # Paths, from where to load the input files (recommended to leave the folder names as they are).
-# Folder "map_files" already exists, only the folder itself and the md file inside it are tracked by Git.
-LANES_GEOJSON_FILE_PATH = "map_files/Lane polygons.geojson"
-INTERSECTIONS_GEOJSON_FILE_PATH = "map_files/Intersection polygons.geojson"
-OSM_XML_FILE_PATH = "map_files/osm.xml"
+# Folder "input_files" already exists, only the folder itself and the md file inside it are tracked by Git.
+LANES_GEOJSON_FILE_PATH = "input_files/Lane polygons.geojson"
+INTERSECTIONS_GEOJSON_FILE_PATH = "input_files/Intersection polygons.geojson"
+OSM_XML_FILE_PATH = "input_files/osm.xml"
 
 # Paths, where to save the output files (recommended to leave the folder names as they are).
 # Folders are created automatically if they don't exist yet. 
