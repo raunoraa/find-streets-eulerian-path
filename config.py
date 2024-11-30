d703e6af6d7bc14413dca3b52dc9be8180d84e46
Rauno Raa <raunoraa13@gmail.com>
2024-11-30 19:45:04 +0200

    Minor changes.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configuration file for file paths and constants used throughout the application.
 
-# Paths, from where to load the input files.
+# Paths, from where to load the input files (recommended to leave the folder names as they are).
 LANES_GEOJSON_FILE_PATH = "map_files/Lane polygons.geojson"
 INTERSECTIONS_GEOJSON_FILE_PATH = "map_files/Intersection polygons.geojson"
 OSM_XML_FILE_PATH = "map_files/osm.xml"
 
-# Paths, where to save the output files.
+# Paths, where to save the output files (recommended to leave the folder names as they are).
 OUTPUT_GPKG_FILE_PATH = "output/output.gpkg"
 OUTPUT_TXT_FILE_PATH = "output/results.txt"
---
+++ input_files/README.md
@@ -0,0 +1,5 @@
+# input_files Folder
+
+This folder is used for storing input files.  
+Files in this folder are ignored by Git to prevent pushes of large data.
+Only this README is tracked by Git.
